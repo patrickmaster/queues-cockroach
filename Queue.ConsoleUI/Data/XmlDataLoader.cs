94542f1357fe61d5190340c11295b0e7df0f8762
KTraczyk <traczyk92@gmail.com>
2015-12-13 13:24:19 +0100

    Implemented LoadInputForJackson

--- Queue.ConsoleUI/Data/XmlDataLoader.cs
+++ Queue.ConsoleUI/Data/XmlDataLoader.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using Queue.Algorithm.Data;
 
 namespace Queue.ConsoleUI.Data
@@ -11,7 +13,53 @@ namespace Queue.ConsoleUI.Data
     {
         public Input LoadInputForJackson(string filename)
         {
-            throw new NotImplementedException();
+            XmlDocument JacksonInput = new XmlDocument();
+            JacksonInput.Load(filename);
+            Input input = new Input();
+            var nodeLambda = JacksonInput.SelectSingleNode("/Jackson/Lambda");
+            if (nodeLambda != null)
+            {
+                input.Lambda = Double.Parse(nodeLambda.InnerText.Trim());             
+            }
+            else
+            {
+                throw new Exception("Wrong xml file format");
+            }
+            var nodeMi = JacksonInput.SelectSingleNode("/Jackson/Mi");
+            if (nodeMi != null)
+            {
+                string[] miValues = nodeMi.InnerText.Trim().Split(';');
+                input.Mi = new double[miValues.Length];
+                for (int i = 0; i < miValues.Length; i++)
+                {
+                    input.Mi[i] = Double.Parse(miValues[i]);
+                }
+            }
+            else
+            {
+                throw new Exception("Wrong xml file format");
+            }
+            var nodeP = JacksonInput.SelectSingleNode("/Jackson/P");
+            if (nodeP != null)
+            {
+                string[] pRows = nodeP.InnerText.Trim().Split('\n');
+                input.P = new double[pRows.Length][];
+                for (int i = 0; i < pRows.Length; i++)
+                {
+                    var row = pRows[i];
+                    string[] pValues = row.Trim().Split(';');
+                    input.P[i] = new double[pValues.Length];
+                    for (int j = 0; j < pValues.Length; j++)
+                    {
+                        input.P[i][j] = Double.Parse(pValues[j]);
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("Wrong xml file format");
+            }
+            return input;
         }
 
         public BcmpInput LoadInputForBcmp(string filename)
--- Queue.ConsoleUI/Program.cs
+++ Queue.ConsoleUI/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Queue.Algorithm.Data;
+using Queue.ConsoleUI.Data;
 
 namespace Queue.ConsoleUI
 {
@@ -10,7 +13,28 @@ namespace Queue.ConsoleUI
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("siemka hopaki");
+            try
+            {
+                Console.WriteLine("Add xml files to JacksonInput folder to use them");
+                Console.WriteLine("Available xml files:");
+                string path = Directory.GetCurrentDirectory();
+                path = path.Remove(path.Length - 9);
+                path = path + "JacksonInput";
+                string[] fileEntries = Directory.GetFiles(path, "*.xml");
+                foreach (var file in fileEntries)
+                {
+                    Console.WriteLine(Path.GetFileName(file));
+                }
+                Console.WriteLine("Which file do you want to use? Type name with extension");
+                var fileToOpen = Console.ReadLine();
+                XmlDataLoader xmlDataLoader = new XmlDataLoader();
+                Input testInput = xmlDataLoader.LoadInputForJackson(path + "\\" + fileToOpen);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
         }
     }
 }
